c442f2b1af28773b3e76af931d435aa415db5314
Niclas-L <lemstromniclas@gmail.com>
2023-12-18 23:09:35 +0200

    Finished 3_6

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.post("/api/persons", (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = {
     name: request.body.name,
     number: request.body.number,

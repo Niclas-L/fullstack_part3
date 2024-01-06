9bf3909e9aa588e72f5e8c33655f085c73884618
Niclas-L <lemstromniclas@gmail.com>
2024-01-06 15:42:59 +0200

    Finished 3_7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   {
@@ -62,7 +64,7 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.post("/api/persons", (request, response) => {
-  const body = request.body
+  const body = request.body;
 
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -70,7 +72,7 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.find(person => person.name === body.name)) {
+  if (persons.find((person) => person.name === body.name)) {
     return response.status(400).json({
       error: "name must be unique",
     });

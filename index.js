9f9e9a908f82bb2209ffb7265f3c76257ae1953c
Niclas-L <lemstromniclas@gmail.com>
2024-01-07 13:36:41 +0200

    Finished 3_13

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(express.json());
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :req[body]"
-  )
-);
+const Person = require("./models/person");
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
 
 const generateId = () => {
   const min = 10;
@@ -42,7 +31,9 @@ const generateId = () => {
 };
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then(person => {
+    response.json(person);
+  })
 });
 
 app.get("/info", (request, response) => {
@@ -95,7 +86,9 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log("Server running on port ${PORT}");
+  console.log(`Server running on port ${PORT}`);
 });
